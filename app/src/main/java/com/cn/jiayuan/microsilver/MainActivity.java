34b6a8357f075c0e85138b7aec58c0dfec5d7054
Tiger <atlantisspeed@gmail.com>
2017-07-18 15:37:58 +0800

    添加依赖库

--- app/src/main/java/com/cn/jiayuan/microsilver/MainActivity.java
+++ app/src/main/java/com/cn/jiayuan/microsilver/MainActivity.java
@@ -5,10 +5,26 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.message)
+    TextView message;
+    @BindView(R.id.content)
+    FrameLayout content;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+    @BindView(R.id.container)
+    LinearLayout container;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -36,10 +52,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @OnClick({R.id.message, R.id.content, R.id.navigation, R.id.container})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.message:
+                Toast.makeText(this, "555555555555", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.content:
+                break;
+            case R.id.navigation:
+                break;
+            case R.id.container:
+                break;
+        }
+    }
 }
